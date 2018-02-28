851aedc4d3b9fbb0a17eaf80d151dc0f3326de6b
rworden <rossworden@gmail.com>
2018-02-28 02:36:23 -0800

    nits

--- global.R
+++ global.R
@@ -1,4 +1,4 @@
 library(shiny)
-setwd("/Users/rworden/courseraDSCapstone/courseraDSCapstone")
+setwd("/Users/rworden/Desktop/ds_hold/courseraDSCapstone")
 source('data_cleaning.R')
 source('backoff_model.R')
